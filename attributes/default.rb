610e2d3ce3f2285144eb300efdb0c893e5b2118a
Chauncey <pairingmachine@rallydev.com>
2013-06-19 15:40:22 -0600

    removing property that isnt needed

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,6 @@ default['grails']['version'] = "2.1.1"
 default['grails']['archive_name']  = "grails-#{node['grails']['version']}.zip"
 default['grails']['source_url']  = "http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/#{node['grails']['archive_name']}"
 default['grails']['path']  = "/usr/local/grails-#{node['grails']['version']}"
-default['grails']['file_path']  = "#{node['grails']['install_path']}.zip"
 default['grails']['owner']  = 'root'
 default['grails']['group']  = 'root'
 default['grails']['mode'] = 755
